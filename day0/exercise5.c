fdd11398e7317e9227ae1cd725b1d05a397b0368
Daniel Ems <danielems3@outlook.com>
2016-10-17 14:57:21 -0400

    Printf Hello

---
+++ day0/exercise5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+		
+	printf("H H EEE L    L    OOO\n");
+	printf("H H E   L    L    O O\n");
+	printf("HHH EEE L    L    O O\n");
+	printf("H H E   L    L    O O\n");
+	printf("H H EEE LLL  LLL  OOO\n");
+
+}
