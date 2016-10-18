c05180a6ae9a16a3297ffdba302f382c41f6992e
Daniel Ems <danielems3@outlook.com>
2016-10-18 11:57:20 -0400

    demo announcing functionis

---
+++ day1/func-demo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int x, int y);
+
+int main(void)
+{
+	int b = 3, c = 5;
+	
+	int a = add(b,c);
+
+	printf("%d\n", a);
+}
+
+
+int add(int x, int y)
+{
+	return x+y;
+}
