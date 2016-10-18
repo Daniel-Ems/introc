4690d0b1cb6f1125ba42f6fae4b902cac53731a1
Daniel Ems <danielems3@outlook.com>
2016-10-18 11:56:01 -0400

    demo arrays

---
+++ day1/array-demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int grades[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int absents[] = {10,9,8,7,6,5,4,3,2,1};
+
+	printf("grades[4] = %d\n", grades[4]);
+	printf("absents[0] = %d\n", absents [0]);
+	
+	
+}
