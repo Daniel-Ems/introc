12f5fd81cfaf923af49083d9bc0bf482a7c6966c
Daniel Ems <danielems3@outlook.com>
2016-10-19 10:12:34 -0400

    Demonstrate f, else if, and else statements

---
+++ day2/birthdays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year = 1969;
+
+	// note that we cuddle the braces of the first part of
+	// the conditional with any additional parts
+
+	if(year > 2000) {
+	 printf("Your are just out of diapers\n");
+	}else if(year > 1990){
+	 puts("I bet you had an extreeeeeme childhood, dude");
+	}else{
+	 puts("You are an old fart");
+	}
+
+
+
+}
