a47cc08173d2768888b6a8d0b3edd70fdaef4209
Daniel Ems <danielems3@outlook.com>
2016-10-20 15:17:38 -0400

    example of switch/case

---
+++ day3/goto2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int days_in_month(int month);
+int main(int argc, char *argv[])
+{
+	if(argc != 2) {
+	  printf("Usage: %s <value>\n", argv[0]);
+	  return 1;
+	}
+
+	int month = strtol(argv[1], NULL, 10);
+	int days = days_in_month(month);
+	
+	printf("There are %d days in the month %d\n", days, month);
+
+}
+
+int days_in_month(int month)
+{
+	int days = 0 ;
+	switch(month) {
+	  case 4:
+	  case 6:
+	  case 9:
+	  case 11:
+		days =  30;
+		break;
+	  default:
+		days = 31;
+	}
+	return days; 
+}
